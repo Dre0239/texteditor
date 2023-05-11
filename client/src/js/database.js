e7009cbc8391b495ffcc2f103ad95aa753e84b10
Dredog0239 <almays29@gmail.com>
2023-05-10 18:37:31 -0700

    updated database.js & install.js. in the client folder.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    await tx.objectStore('jate').add({ content });
+    await tx.done;
+    console.log('Content added to database:', content);
+  } catch (error) {
+    console.error('Error adding content to database:', error);
+  }
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const cursor = await tx.objectStore('jate').openCursor();
+    const content = [];
+    while (cursor) {
+      content.push(cursor.value.content);
+      cursor.continue();
+    }
+    await tx.done;
+    console.log('Content retrieved from database:', content);
+    return content;
+  } catch (error) {
+    console.error('Error retrieving content from database:', error);
+    return [];
+  }
+};
+
 
 initdb();
